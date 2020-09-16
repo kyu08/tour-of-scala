cac4f704f439b1e530fa65ae6c44160ec4d61f89
kyu08 <kyu08@users.noreply.github.com>
2020-09-17 00:31:25 +0900

    trait

--- src/main/scala/TourOfScala.scala
+++ src/main/scala/TourOfScala.scala
@@ -1,5 +1,27 @@
 object TourOfScala extends App {
   println("===================")
-  println("joge")
+  trait Iterator[A] {
+    def hasNext: Boolean
+    def next(): A
+  }
+
+  class IntIterator(to: Int) extends Iterator[Int] {
+    private var current = 0
+
+    override def hasNext: Boolean = current < to
+
+    override def next(): Int = {
+      if (hasNext) {
+        val t = current
+        current += 1
+        t
+      } else 0
+    }
+  }
+
+  val iterator = new IntIterator(10)
+  println(iterator.next())
+  println(iterator.next())
+  println(iterator.next())
   println("===================")
 }
